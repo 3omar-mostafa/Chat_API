e727c289af2f0afe2e15f3006e2e21033511fff4
3omar-mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2022-10-22 13:11:43 +0200

    [Github Actions] Added CI Pipeline

--- docker/entrypoints/test.sh
+++ docker/entrypoints/test.sh
@@ -4,4 +4,4 @@ rm -f tmp/pids/server.pid
 unset DATABASE_URL # if DATABASE_URL is set, it will override the database.yml config and use production database
 export RAILS_ENV=test
 bundle exec rails db:setup
-bundle exec rspec
+bundle exec rspec --color --tty
